d63d980073c2ca36609fb254cb34b2b78ad221c9
Marfullsen <marfullsen@gmail.com>
2021-05-30 22:25:22 -0400

    JSON crud added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+from os import path
 from flask import Flask, render_template, request, redirect, url_for
 from static import minimap
 
 app = Flask(__name__)
+json_filename = 'recs_info.json'
 
 @app.route('/')
 def index():
@@ -21,14 +23,30 @@ def upload_file():
                             source='minimap_'+uploaded_file.filename[:-4]+'.png', 
                             info_json=info_json)
 
-def write(info_json):
-    """Creates a Json file"""
-    pass
+def json_crud(json_filename):
+    if not path.exists(json_filename):
+        create_new_file()
+    try:
+        data = read_file(json_filename)
+    except:
+        print('\nError\n¡Archivo dañado!\n')
 
-def read():
-    """Reads a Json file"""
-    pass
+def read_file(json_filename):
+    """Reads a json file"""
+    with open(json_file_name) as json_data:
+        data = json.load(json_data)
+    return data
+
+def write_to_file():
+    """Creates a json file"""
+    with open(json_file_name, 'w') as outfile:
+        json.dump(data, outfile)
+
+def create_new_file(json_filename):
+    json_data = open(json_filename, 'w')
+    write_to_file()
+    json_data.close()
 
 if __name__ == '__main__':
-    read()
-   app.run(debug = True)
+    json_crud(json_filename)
+    app.run(debug = True)
