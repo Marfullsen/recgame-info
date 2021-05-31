50fdb72222c747d52da2f25828470c5a68e39da6
Marfullsen <marfullsen@gmail.com>
2021-05-31 16:43:22 -0400

    JSON API implemented

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import json, os
 from os import path
-from flask import Flask, render_template, request, redirect, url_for
+#.#.#
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+#.#.#
 from static import minimap
 
 app = Flask(__name__)
+data = list()
 json_filename = 'recs_info.json'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get_data')
+def get_data():
+    return jsonify(data)
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -17,36 +25,59 @@ def upload_file():
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
         info_json = minimap.write_minimap(uploaded_file.filename)
-        write(info_json)
+        info_json['minimap'] = f'static/minimap_{info_json["nombre_archivo"][:-4]}.png'
+        write_to_file(json_filename, data, info_json)
+        os.rename(uploaded_file.filename,f'./recs/{uploaded_file.filename}')
     return render_template('generated.html', 
                             filename=uploaded_file.filename,
                             source='minimap_'+uploaded_file.filename[:-4]+'.png', 
                             info_json=info_json)
 
 def json_crud(json_filename):
+    """init the json file.
+    Si el archivo no existe, se creará.
+    Si el archivo existe, se intentará abrir.
+    Si el archivo está dañado, se hará un backup
+    del archivo dañado, y se creará un nuevo archivo.
+    """
     if not path.exists(json_filename):
-        create_new_file()
+        data = create_new_file(json_filename)
+        return data
     try:
         data = read_file(json_filename)
+        return data
     except:
         print('\nError\n¡Archivo dañado!\n')
+        import os
+        os.rename(json_filename, json_filename+'_')
+        return list()
 
 def read_file(json_filename):
     """Reads a json file"""
-    with open(json_file_name) as json_data:
+    with open(json_filename) as json_data:
         data = json.load(json_data)
     return data
 
-def write_to_file():
+def write_to_file(json_filename, data, new_info):
     """Creates a json file"""
-    with open(json_file_name, 'w') as outfile:
+    if new_info:
+        data.append(new_info)
+    with open(json_filename, 'w') as outfile:
         json.dump(data, outfile)
 
 def create_new_file(json_filename):
+    """Creates a 'recs_info.json' file"""
+    data = list()
     json_data = open(json_filename, 'w')
-    write_to_file()
+    write_to_file(json_filename, data, '')
     json_data.close()
+    return data
+
+def init_folders():
+    if not path.exists('recs'):
+        os.mkdir('recs')
 
 if __name__ == '__main__':
-    json_crud(json_filename)
+    data = json_crud(json_filename)
+    init_folders()
     app.run(debug = True)
